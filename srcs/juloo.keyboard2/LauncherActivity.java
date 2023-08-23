308ae2acba54fdff3d8997b3c63949974d283a55
RetrogisusDEV <99091223+RetrogisusDEV@users.noreply.github.com>
2023-08-23 18:53:31 -0400

    Add InputMethodPicker to LauncherActivity

--- srcs/juloo.keyboard2/LauncherActivity.java
+++ srcs/juloo.keyboard2/LauncherActivity.java
@@ -9,6 +9,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.inputmethod.*;
 
 public class LauncherActivity extends Activity
 {
@@ -31,6 +32,12 @@ public class LauncherActivity extends Activity
   public void launch_imesettings(View _btn)
   {
     startActivity(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS));
+  }
+
+  public void launch_imepicker(View v)
+  {
+    InputMethodManager IMM = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+			IMM.showInputMethodPicker();
   }
 
   final class Tryhere_OnUnhandledKeyEventListener implements View.OnUnhandledKeyEventListener
